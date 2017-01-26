4a65da37b92695b5470af5a147508ddc135862b6
ruby 27 <ruby.27@metralha.cec.ime.usp.br>
2017-01-26 19:38:55 -0200

    Commit Inicial

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Actor.create!(name: "Angelina Jolie", gender:"female", birthdate: "07-Mar-1985")
+Actor.create!(name: "Will Smith", gender:"male", birthdate: "16-Jun-1975")
+Actor.create!(name: "Margot Robbie", gender:"female", birthdate: "8-Jan-1965")
+
+Director.create!(name: "Quentin Tarantino", gender: "male", birthdate: "25-sep-1952")
+Director.create!(name: "Mel Gibson", gender: "male", birthdate: "3-Jan-1936")
+
 movies = [
   {title: "Alladin", release_date: '25-Nov-1992'},
   {title: "When Harry Met Sally", release_date: '21-Jul-1989'},
